2b7b0a55c0943eefee7cedeefbab047da4b281d1
dsme globallinks <hr3261331@gmail.com>
2024-07-03 19:25:06 +0500

    new chnages

---
+++ src/Constants/Colors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BLACK: 'black',
+  WHITE: 'white',
+  LIGHTGREY: '#F2F2F3',
+  DARKGREY: '#AFB0B6',
+  GREEN: '#01C96C',
+};
--- src/Screens/JobPostScreen.js
+++ src/Screens/JobPostScreen.js
@@ -1,17 +1,167 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import Header from '../components/header';
+import {DARKGREY, GRAY, GREEN, LIGHTGREY, WHITE} from '../Constants/Colors';
 
 export default function JobPostScreen({navigation}) {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'red',
+        backgroundColor: WHITE,
       }}>
-      <Header />
+      <Header Heading={'Post a Job'} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            backgroundColor: LIGHTGREY,
+            padding: 20,
+            flexDirection: 'row',
+          }}>
+          <View>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  backgroundColor: GREEN,
+                  height: 25,
+                  width: 30,
+                  borderRadius: 13,
+                }}
+              />
+              <View
+                style={{
+                  borderWidth: 0.5,
+                  borderColor: DARKGREY,
+                  width: '42%',
+                  height: 0,
+                  alignSelf: 'center',
+                }}
+              />
+            </View>
+
+            <Text style={{color: GREEN, marginTop: '3%', fontSize: 12}}>
+              Job Detail
+            </Text>
+          </View>
+          <View
+            style={{
+              marginLeft: '-21%',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  height: 25,
+                  width: 25,
+                  borderRadius: 13,
+                  borderWidth: 1,
+                  borderColor: DARKGREY,
+                }}
+              />
+              <View
+                style={{
+                  borderWidth: 0.5,
+                  borderColor: DARKGREY,
+                  width: '42%',
+                  height: 0,
+                  alignSelf: 'center',
+                }}
+              />
+            </View>
+
+            <Text
+              style={{
+                color: DARKGREY,
+                marginTop: '3%',
+                fontSize: 12,
+                marginLeft: '-7%',
+              }}>
+              Post Detail
+            </Text>
+          </View>
+          <View
+            style={{
+              marginLeft: '-27%',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  height: 25,
+                  width: 25,
+                  borderRadius: 13,
+                  borderWidth: 1,
+                  borderColor: DARKGREY,
+                }}
+              />
+              <View
+                style={{
+                  borderWidth: 0.5,
+                  borderColor: DARKGREY,
+                  width: '42%',
+                  height: 0,
+                  alignSelf: 'center',
+                }}
+              />
+            </View>
+
+            <Text
+              style={{
+                color: DARKGREY,
+                marginTop: '3%',
+                fontSize: 12,
+                marginLeft: '-7%',
+              }}>
+              Post Detail
+            </Text>
+          </View>
+          <View
+            style={{
+              marginLeft: '-28%',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  height: 25,
+                  width: 25,
+                  borderRadius: 13,
+                  borderWidth: 1,
+                  borderColor: DARKGREY,
+                }}
+              />
+              <View
+                style={{
+                  // borderWidth: 0.5,
+                  // borderColor: DARKGREY,
+                  width: '42%',
+                  height: 0,
+                  alignSelf: 'center',
+                }}
+              />
+            </View>
+
+            <Text
+              style={{
+                color: DARKGREY,
+                marginTop: '3%',
+                fontSize: 12,
+                marginLeft: '-7%',
+              }}>
+              Post Detail
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
